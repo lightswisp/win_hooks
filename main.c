7dff3ddfb4c3c29b5a1106d4bbc9e6a789cfbc60
lightswisp <48927861+lightswisp@users.noreply.github.com>
2024-06-19 08:08:56 -0400

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,16 @@
 #include <string.h>
 #include <windows.h>
 
+#define PATCH_SIZE 12
+
+typedef struct{
+    void* orig_addr;
+    unsigned long old_protect;
+    unsigned int orig[PATCH_SIZE];
+} Hook;
+
+static Hook hk;
+
 void print_error(){
     printf("[-] Error has occured - LastError: %lu\n", GetLastError());
 }
@@ -52,6 +62,7 @@ unsigned char* generate_patch(void* hook_addr, size_t* size){
     // 7:  11 00 00
     // a:  50                      push   rax
     // b:  c3                      ret
+
     unsigned char temp[] = {
         0x48, 0xb8, 0x00, 0x00, 0x00, 0x00,0x00, 0x00, 0x00, 0x00, //movabs rax,0x00000000000
         0x50,                                                                                         //push rax
@@ -75,22 +86,32 @@ unsigned char* generate_patch(void* hook_addr, size_t* size){
     return jump_buffer;
 }
 
-void install_hook(void* orig_addr, void* hook_addr){
+void init_hook(){
+    unsigned int temp[PATCH_SIZE] = {0,0,0,0,0,0,0,0,0,0,0,0};
+    hk.orig_addr        = 0;
+    hk.old_protect      = 0;
+    memcpy(hk.orig, temp, PATCH_SIZE);
+}
+
+void install_hook(void* hook_addr){
     size_t patch_size = 0;
     unsigned char* patch = generate_patch(hook_addr, &patch_size);
     debug_dump(patch, patch_size);
 
-    unsigned long old_protect;
-    if(VirtualProtectEx(GetCurrentProcess(), orig_addr, patch_size, PAGE_EXECUTE_READWRITE, &old_protect) == 0){
+    if(VirtualProtectEx(GetCurrentProcess(), hk.orig_addr, patch_size, PAGE_EXECUTE_READWRITE, &hk.old_protect) == 0){
         print_error();
         exit(EXIT_FAILURE);
     }
-    memcpy(orig_addr, patch, patch_size);
+    memcpy(hk.orig_addr, patch, patch_size);
 }
 
 void remove_hook(){
-    // todo
-    // old_protect is needed
+    unsigned long old_protect;
+    memcpy(hk.orig_addr, hk.orig, PATCH_SIZE);
+    if(VirtualProtectEx(GetCurrentProcess(), hk.orig_addr, PATCH_SIZE, hk.old_protect, &old_protect) == 0){
+        print_error();
+        exit(EXIT_FAILURE);
+    }
 }
 
 //////////////////////////////////////////////////
@@ -98,51 +119,50 @@ void remove_hook(){
 
 int messagebox_hook(HWND hWnd, LPCSTR lpText, LPCSTR lpCaption, UINT uType){
 
-    /* 
-        4) OUR HOOK FUNCTION SHOULD LOOK LIKE THIS
+    // printf("[*] From hook => MessageBoxA was called!\n");
+    // printf("hwnd: %p\nlptext: %s\nlpcaption: %s\nutype: %u", hWnd, lpText, lpCaption, uType);
+    remove_hook();
 
-    patched_func:
-	... => hook logic 
-	55           push   rbp			                    => this is from the original function
-	48 89 e5     mov    rbp,rsp		                    => this is from the original function
-	48 83 ec 10  sub    rsp,0x10		                => this is from the original function
-	             jmp original_func + temp_buffer_size	=> jump back to the original
-    */
+    lpText = "Hooked";
+    lpCaption = "Hooked";
+    uType    = MB_HELP;
+    
+    MessageBoxA(hWnd, lpText, lpCaption, uType);
+    install_hook(messagebox_hook);
 
-    printf("[*] From hook => MessageBoxA was called!\n");
-    printf("hwnd: %u\nlptext: %s\nlpcaption: %s\nutype: %u", hWnd, lpText, lpCaption, uType);
-    // __asm__{
-    //     //after func logic
-    // }
-    return 0; // replace with original messagebox + offset
+    return 0; 
 }
 
 int main(void){
+
     void* messagebox_orig = get_remote_func_addr("user32.dll", "MessageBoxA");
     if(NULL == messagebox_orig)
         exit(EXIT_FAILURE);
 
-    // __asm__{
-    //     movabs rax, 0x1122334455667788
-    //     push rax
-    //     ret
-    // }
+    init_hook();
+    hk.orig_addr = messagebox_orig;
 
+    printf("%p\n", hk.orig_addr);
+    memcpy(hk.orig, retain_orig(hk.orig_addr, PATCH_SIZE), PATCH_SIZE);
 
-    printf("hi %llX\n", messagebox_orig);
-    printf("messagebox_hook: %llX\n", messagebox_hook);
+    printf("hi %p\n", hk.orig_addr);
+    printf("messagebox_hook: %p\n", messagebox_hook);
 
     puts("MessageBoxA before hook:");
-    debug_dump(messagebox_orig, 100);
+    debug_dump(hk.orig_addr, 100);
 
     MessageBoxA(0, "Test", "Test", MB_OK);
     
-    install_hook(messagebox_orig, messagebox_hook);
+    install_hook(messagebox_hook);
 
     puts("MessageBoxA after hook:");
     debug_dump(messagebox_orig, 100);
 
     MessageBoxA(0, "Test", "Test", MB_OK);
+    MessageBoxA(0, "Test", "Test", MB_OK);
 
+    remove_hook();
+
+    MessageBoxA(0, "Test", "Test", MB_OK);
     return 0;
 }
